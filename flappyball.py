2fe1fd3d94d39da28d0c078d00c8a92b3fd3d168
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-04-22 20:29:43 +0530

    Add files via upload

---
+++ flappyball.py
@@ -0,0 +1,42 @@
+import pgzrun
+
+WIDTH=800
+HEIGHT=600
+
+class ball:
+    GRAVITY=2000
+    def __init__(self,color,size,startx,starty):
+        self.color=color
+        self.size=size
+        self.x=startx
+        self.y=starty
+        self.yvelocity=0
+        
+    def draw(self):
+        screen.draw.filled_circle((self.x,self.y),self.size,self.color)
+
+ball1=ball("orange",40,700,20)
+
+
+def draw():
+    screen.clear()
+    screen.fill("black")
+    ball1.draw()
+
+def update(dt):
+    initialvelocity=ball1.yvelocity
+    ball1.yvelocity=initialvelocity+ball1.GRAVITY*dt #v=u+at
+    s=(initialvelocity+ball1.yvelocity)/2*dt
+    ball1.y=ball1.y+s
+    if ball1.y>=HEIGHT:
+        ball1.yvelocity=ball1.yvelocity*-0.89
+
+def on_key_down(key):
+    if key==keys.SPACE:
+        ball1.yvelocity=-500
+
+
+
+
+
+pgzrun.go()
