2b138daadc82648636749318826f3aca5fbabf84
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-05-20 19:59:48 +0530

    Imagerotation.py

--- Imagerotation.py
+++ Imagerotation.py
@@ -15,20 +15,27 @@ image2=pygame.image.load("Image2.jpg")
 image2=pygame.transform.scale(image2,(WIDTH,HEIGHT))
 image3=pygame.image.load("Image3.jpg")
 image3=pygame.transform.scale(image3,(WIDTH,HEIGHT))
+
+font1=pygame.font.SysFont("Arial",63)
+text1=font1.render("Happy Birthday", True, (255,0,0))
+text2=font1.render("Have fun", True, (0,255,0))
 while run==True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
             pygame.quit()
     screen.blit(image1,(0,0))
+    screen.blit(text1,(500,400))
     pygame.display.update()
-    time.sleep(3)
+    time.sleep(3)   
     screen.blit(image2,(0,0))
+    screen.blit(text2,(300,200))
     pygame.display.update()
     time.sleep(3)
     screen.blit(image3,(0,0))
     pygame.display.update()
     time.sleep(3)
+
     
 
 
