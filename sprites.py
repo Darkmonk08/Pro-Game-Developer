0c3c5e3e588717eb4e61fd7df2574d253f9474ed
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-07-01 20:26:08 +0530

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+
+WIDTH=800
+HEIGHT=600
+
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+run=True
+
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self,rocketx,rockety):
+        super().__init__()
+        self.image=pygame.image.load("Image7.png")
+        self.rect=self.image.get_rect()
+        self.rect.center=(rocketx,rockety)
+
+image1=pygame.image.load("Image6.png")
+image1=pygame.transform.scale(image1,(WIDTH,HEIGHT))
+
+rockets=pygame.sprite.Group()
+rocket1=Rocket(150,100)
+rockets.add(rocket1)
+rocket2=Rocket(400,300)
+rockets.add(rocket2)
+
+while run==True:
+    screen.blit(image1,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+            pygame.quit()
+    rockets.draw(screen)
+    pygame.display.update()
+    
+
+
+
+    
