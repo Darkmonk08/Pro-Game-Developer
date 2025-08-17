4d058c051fca44509c36b74ab6ad143f0ee2472c
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-08-17 15:29:28 +0530

    sprites.py

--- sprites.py
+++ sprites.py
@@ -14,12 +14,16 @@ class Rocket(pygame.sprite.Sprite):
         self.image=pygame.image.load("Image7.png")
         self.rect=self.image.get_rect()
         self.rect.center=(rocketx,rockety)
+    def update(self):
+        self.rect.y=self.rect.y-3
+
+
 
 image1=pygame.image.load("Image6.png")
 image1=pygame.transform.scale(image1,(WIDTH,HEIGHT))
 
 rockets=pygame.sprite.Group()
-rocket1=Rocket(150,100)
+rocket1=Rocket(150,600)
 rockets.add(rocket1)
 rocket2=Rocket(400,300)
 rockets.add(rocket2)
@@ -31,6 +35,7 @@ while run==True:
             run=False
             pygame.quit()
     rockets.draw(screen)
+    rockets.update()
     pygame.display.update()
     
 
